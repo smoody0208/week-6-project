eb2580c7f50fc3f789b45d5f97d23e8c10e06392
Spencer Moody <spencer.moody@outlook.com>
2020-07-03 11:16:53 -0700

    Import Converter into main.js and receive inputs for amount and country from user

--- src/main.js
+++ src/main.js
@@ -2,3 +2,14 @@ import $ from "jquery";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { Converter } from './currency-converter.js';
+
+document.readyState(function() {
+  $("#calculate").click(function(event) {
+    event.preventDefault()
+    let amount = $("#amount").val();
+    let currencyType = $("#country").val();
+    $("#amount").val("");
+    $("#country").val("");
+  })
+})

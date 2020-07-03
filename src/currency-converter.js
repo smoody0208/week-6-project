c823ebb688de6e1fd1a929e669e0d308a9bc878f
Spencer Moody <spencer.moody@outlook.com>
2020-07-03 09:36:33 -0700

    Add try catch for error

--- src/currency-converter.js
+++ src/currency-converter.js
@@ -1,12 +1,15 @@
 export class Converter{
   async callApi() {
-    const response = await fetch (`https://v6.exchangerate-api.com/v6/%{process.env.API_KEY}/latest/USD`);
-    let jsonifiedResponse;
-    if (response.ok && response.status == 200) {
-      jsonifiedResponse = await response.json();
-    } else {
-      jsonifiedResponse = false
-    }
+    try {
+      const response = await fetch (`https://v6.exchangerate-api.com/v6/%{process.env.API_KEY}/latest/USD`);
+      let jsonifiedResponse;
+      if (response.ok && response.status == 200) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false
+      }
+      return jsonifiedResponse;
+    } catch(error) {
   }
+ }
 }
-

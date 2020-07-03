e25967bcd70857a0386d043e76b475bd162f803e
Spencer Moody <spencer.moody@outlook.com>
2020-07-03 15:55:20 -0700

    Refactor code and delete currencyConverter

--- src/currency-converter.js
+++ src/currency-converter.js
@@ -1,25 +1,9 @@
-export async function converter() {
-    try {
-      const response = await fetch (`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
-      let jsonifiedResponse;
-      if (response.ok && response.status == 200) {
-        jsonifiedResponse = await response.json();
-      } else {
-        jsonifiedResponse = false
-      }
-      return jsonifiedResponse;
-    } catch(error) {
-      console.log(error);
-      return false;
-    }
-  }
-
-  async function toFromConversition(fromCountry) {
-    try {
-      let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/${fromCountry}`);
-      let jsonifiedResponse;
-      if (response.ok && response.status == 200) {
-        jsonifiedResponse = await response.json();
+export async function toFromConversition() {
+  try {
+    let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/USD`);
+    let jsonifiedResponse;
+    if (response.ok && response.status == 200) {
+    jsonifiedResponse = await response.json();
       } else {
         jsonifiedResponse = false;
       }
--- src/main.js
+++ src/main.js
@@ -2,18 +2,19 @@ import $ from "jquery";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { converter } from './currency-converter.js';
+import { toFromConversition } from './currency-converter.js';
 
-async function currencyConverter(amount, country) {
-  const jsonifiedResponse = await converter();
+async function currencyConverter2(amount, country, country2) {
+  const jsonifiedResponse = await toFromConversition();
   if (jsonifiedResponse === false) {
     $("#output").text("I'm sorry, something went wrong wtih your request");
-  } else if (!jsonifiedResponse.conversion_rates[country]) {
+  } else if (!jsonifiedResponse.conversion_rates) {
     $(".showOutput").html(`<p>Change currency conversion</p>${jsonifiedResponse.error}`);
   } else {
-    let convertTo = jsonifiedResponse.conversion_rates[country];
-    let exchange = (amount * convertTo);
-    $("#output").html(exchange + country);
+    let convertTo = jsonifiedResponse.conversion_rates[country] / jsonifiedResponse.conversion_rates[country2];
+    let exchange = amount * convertTo;
+    console.log(jsonifiedResponse.conversion_rates[country2]);
+    $("#output").html(`${exchange}`);
   }
 }
 $(document).ready(function() {
@@ -21,9 +22,10 @@ $(document).ready(function() {
     event.preventDefault()
     let amount = parseInt($("#amount").val());
     let country = $("#country").val();
+    let country2 = $("#country2").val();
     $("#amount").val("");
     $("#country").val("");
-    $
-    currencyConverter(amount, country);
+    $("#country2").val("")
+    currencyConverter2(amount, country, country2);
   })
 })

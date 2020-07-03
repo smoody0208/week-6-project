45bb7811065a25a45ab61e4da85f95ecaf32f75e
Spencer Moody <spencer.moody@outlook.com>
2020-07-03 14:54:06 -0700

    Allow user to convert between all currencies

--- src/currency-converter.js
+++ src/currency-converter.js
@@ -1,6 +1,6 @@
-export async function converter() {
+export async function converter(amount, country, country2) {
     try {
-      const response = await fetch (`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+      const response = await fetch (`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${country2}/${country}/${amount}`);
       let jsonifiedResponse;
       if (response.ok && response.status == 200) {
         jsonifiedResponse = await response.json();
--- src/main.js
+++ src/main.js
@@ -4,16 +4,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { converter } from './currency-converter.js';
 
-async function currencyConverter(amount, country) {
-  const jsonifiedResponse = await converter();
+async function currencyConverter(amount, country, country2) {
+  const jsonifiedResponse = await converter(amount, country, country2);
   if (jsonifiedResponse === false) {
     $("#output").text("I'm sorry, something went wrong wtih your request");
-  } else if (!jsonifiedResponse.conversion_rates[country]) {
+  } else if (!jsonifiedResponse.conversion_rates[country2]) {
     $(".showOutput").html(`<p>Change currency conversion</p>${jsonifiedResponse.error}`);
   } else {
-    let convertTo = jsonifiedResponse.conversion_rates[country];
+    let convertTo = jsonifiedResponse.conversion_result;
     let exchange = (amount * convertTo);
-    $("#output").html(exchange + country);
+    $("#output").html(exchange + country2);
   }
 }
 $(document).ready(function() {
@@ -21,8 +21,10 @@ $(document).ready(function() {
     event.preventDefault()
     let amount = parseInt($("#amount").val());
     let country = $("#country").val();
+    let country2 = $("#country2").val();
     $("#amount").val("");
     $("#country").val("");
-    currencyConverter(amount, country);
+    $()
+    currencyConverter(amount, country, country2);
   })
 })
